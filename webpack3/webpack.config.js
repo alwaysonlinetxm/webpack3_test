5206bb574f290c2646f7d92fa1a61056243f000a
taoxuming <taoxuming@hys-inc.cn>
2017-06-30 15:49:42 +0800

    add rollup

---
+++ webpack3/webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = function makeWebpackConfig(env) {
+	return {
+	  entry: './src/main.js',
+	  output: {
+	    path: path.join(__dirname, '../dist'),
+	    filename: 'bundle.w2.js'
+	  },
+	  plugins: [
+			new webpack.optimize.UglifyJsPlugin({
+	      compress: {
+	        sequences: true,
+	        dead_code: true,
+	        conditionals: true,
+	        booleans: true,
+	        unused: true,
+	        if_return: true,
+	        join_vars: true,
+	        drop_console: true,
+					drop_debugger: true,
+					loops: true,
+					properties: true
+	      },
+	      output: {
+	        comments: false
+	      }
+	    })
+	  ]
+	};
+}
