2a5fcaf613840ae4bd3e11592f4e2d5f72fa282e
taoxuming <taoxuming@hys-inc.cn>
2017-06-30 17:48:04 +0800

    update shell

--- generate.sh
+++ generate.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 num=$1
+type=$2
 main="./src/main.js"
 data="var content = 0"
 
-if [ -f "./src/modules/module1.js" ]; then
+if [ -f "./src/modules/module1.js" ];
+then
   rm ./src/modules/*
   : > ${main}
 fi
 
-if [ ${num} = '0' ]; then
+if [ ${num} = '0' ];
+then
   exit
 fi
 
 for k in $( seq 1 ${num} )
 do
-  data="${data} + m${k}"
-  echo "import m${k} from './modules/module${k}.js';" >> ${main}
-  echo "export default ${k}" > ./src/modules/module${k}.js
+  if [ ${type} = 'num' ];
+  then
+    data="${data} + m${k}"
+    echo "import m${k} from './modules/module${k}.js';" >> ${main}
+    echo "export default ${k}" > ./src/modules/module${k}.js
+  else
+    data="${data} + m${k}()"
+    echo "import m${k} from './modules/module${k}.js';" >> ${main}
+    echo "export default function() { return ${k} }" > ./src/modules/module${k}.js
+  fi
 done
 
 echo "${data}; document.body.innerHTML = content;" >> ${main}
