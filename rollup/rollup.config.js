8a3332e6c16ef1ab12233c322b608abfd7ed2c19
taoxuming <taoxuming@hys-inc.cn>
2017-06-30 15:56:46 +0800

    upate rolluo

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,10 +1,29 @@
 import filesize from 'rollup-plugin-filesize';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: './src/main.js',
   dest: 'dist/bundle.r.js',
   format: 'iife',
   plugins: [
-    filesize()
+    filesize(),
+    uglify({
+      compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true,
+        drop_debugger: true,
+        loops: true,
+        properties: true
+      },
+      output: {
+        comments: false
+      }
+    })
   ]
 };
