2f9ec87423b3659def489b9e854e6981170fa8f0
taoxuming <taoxuming@hys-inc.cn>
2017-06-30 14:31:56 +0800

    update shell

--- generate.sh
+++ generate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 num=$1
+main="./src/main.js"
 
 if [ -f "./src/modules/module1.js" ]; then
   rm ./src/modules/*
+  : > ${main}
 fi
 
 if [ ${num} = '0' ]; then
@@ -11,5 +13,6 @@ fi
 
 for k in $( seq 1 ${num} )
 do
-   echo "export default ${k}" > ./src/modules/module${k}.js
+  echo "import m${k} from './modules/module${k}.js';" >> ${main}
+  echo "export default ${k}" > ./src/modules/module${k}.js
 done
